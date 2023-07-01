5f2f59f2660a58cad95f07327424cd3bb8a885e0
geckoyuil157 <quangduy15hp07@gmail.com>
2023-07-01 14:54:04 +0700

    Update database

---
+++ database/hokhau.sql
@@ -0,0 +1,23 @@
+INSERT INTO `ho_khau` (`MaHo`, `SoThanhVien`, `DiaChi`)
+VALUES
+  (1, 4, `Ngõ 123, Đường Lạc Long Quân, Quận Hồng Bàng, Hải Phòng`),
+  (2, 3, `Số 10, Đường Lê Lợi, Phường Đông Cương, Thành phố Thanh Hóa, Tỉnh Thanh Hóa`),
+  (3, 3, `Tầng 8, Tòa nhà Dolphin Plaza, Số 6 Nguyễn Hoàng, Quận Ngô Quyền, Hải Phòng`),
+  (4, 2, `Nhà số 15, Khu phố 5, Phường Ba Đình, Thành phố Thanh Hóa, Tỉnh Thanh Hóa`),
+  (5, 2, `Số 12A, Đường Trần Phú, Quận Lê Chân, Hải Phòng`),
+  (6, 2, `Số 20, Đường Trường Thi, Phường Điện Biên, Thành phố Thanh Hóa, Tỉnh Thanh Hóa`),
+  (7, 2, `Nhà số 15, Khu phố 5, Phường Đằng Lâm, Quận Hải An, Hải Phòng`),
+  (8, 2, `Tầng 3, Tòa nhà Vinaconex 3, Số 100, Đường Quang Trung, Thành phố Thanh Hóa, Tỉnh Thanh Hóa`),
+  (9, 3, `Số 10, Đường Lê Hồng Phong, Quận Kiến An, Hải Phòng`),
+  (10, 3, `Ngõ 8, Đường Hàm Rồng, Phường Đông Vệ, Thành phố Thanh Hóa, Tỉnh Thanh Hóa`),
+  (11, 3, `Số 10, Phố Tràng Tiền, Quận Hoàn Kiếm, Hà Nội`),
+  (12, 2, `Số 10, Đường Hồ Chí Minh, Phường Hưng Bình, Thành phố Vinh, Tỉnh Nghệ An`),
+  (13, 3, `Tòa nhà Keangnam Landmark 72, Phạm Hùng, Quận Nam Từ Liêm, Hà Nội`),
+  (14, 2, `Tòa nhà Vincom Plaza Nghệ An, Số 191, Đường Lê Lợi, Phường Hưng Dũng, Thành phố Vinh, Tỉnh Nghệ An`),
+  (15, 2, `Số 20, Ngõ 123, Đường Nguyễn Chí Thanh, Quận Đống Đa, Hà Nội`),
+  (16, 2, `Số 20, Ngõ 123, Đường Quang Trung, Phường Quang Trung, Thành phố Vinh, Tỉnh Nghệ An`),
+  (17, 2, `Tầng 5, Tòa nhà Vincom Center Ba Trieu, Số 191, Bà Triệu, Quận Hai Bà Trưng, Hà Nội`),
+  (18, 2, `Tầng 3, Tòa nhà 789, Số 456, Đường Trần Phú, Phường Hà Huy Tập, Thành phố Vinh, Tỉnh Nghệ An`),
+  (19, 3, `Số 30, Đường Lê Văn Lương, Quận Thanh Xuân, Hà Nội`),
+  (20, 5, `Nhà số 30, Khu phố 5, Xã Nghi Phú, Huyện Nghi Lộc, Tỉnh Nghệ An`);
+
---
+++ database/quanhe.sql
@@ -0,0 +1,73 @@
+INSERT INTO QuanHe (MaHo, IDThanhVien, QuanHe)
+VALUES
+  (1, 1, `Con`),      -- Thành viên có ID là 1 trong hộ khẩu số 1 có quan hệ là "Con"
+  (1, 2, `Con`),      -- Thành viên có ID là 2 trong hộ khẩu số 1 có quan hệ là "Con"
+  (1, 3, `Chủ hộ`),   
+  (1, 4, `Vợ`),       -- Thành viên có ID là 4 trong hộ khẩu số 1 có quan hệ là "Vợ" 
+
+  (2, 5, `Con`),      -- Thành viên có ID là 5 trong hộ khẩu số 2 có quan hệ là "Con"
+  (2, 6, `Chủ hộ`),       
+  (2, 7, `Con`),      -- Thành viên có ID là 7 trong hộ khẩu số 2 có quan hệ là "Con"
+
+  (3, 8, `Vợ`),       -- Thành viên có ID là 8 trong hộ khẩu số 3 có quan hệ là "Vợ"
+  (3, 9, `Chủ hộ`),
+  (3, 10, `Con`),     -- Thành viên có ID là 10 trong hộ khẩu số 3 có quan hệ là "Con"
+
+  (4, 11, `Con`),     -- Thành viên có ID là 11 trong hộ khẩu số 4 có quan hệ là "Con"
+  (4, 12, `Chủ hộ`),  
+
+  (5, 13, `Chồng`),   -- Thành viên có ID là 13 trong hộ khẩu số 5 có quan hệ là "Chồng"
+  (5, 14, `Chủ hộ`),
+  
+  (6, 15, `Chủ hộ`), 
+  (6, 16, `Vợ`),      -- Thành viên có ID là 16 trong hộ khẩu số 6 có quan hệ là "Vợ"
+
+  (7, 17, `Chủ hộ`), 
+  (7, 18, `Vợ`),      -- Thành viên có ID là 18 trong hộ khẩu số 7 có quan hệ là "Vợ"
+
+  (8, 19, `Chủ hộ`),  
+  (8, 20, `Vợ`),      -- Thành viên có ID là 20 trong hộ khẩu số 8 có quan hệ là "Vợ"
+
+  (9, 21, `Em gái `), -- Thành viên có ID là 21 trong hộ khẩu số 9 có quan hệ là "Em gái"
+  (9, 22, `Con`),     -- Thành viên có ID là 22 trong hộ khẩu số 9 có quan hệ là "Con"
+  (9, 23, `Chủ hộ`),
+
+  (10, 24, `Em trai`),-- Thành viên có ID là 24 trong hộ khẩu số 10 có quan hệ là "Em trai"
+  (10, 25, `Con`),    -- Thành viên có ID là 25 trong hộ khẩu số 10 có quan hệ là "Con"
+  (10, 26, `Chủ hộ`),
+     
+  (11, 27, `Em gái`), -- Thành viên có ID là 27 trong hộ khẩu số 10 có quan hệ là "Em gái"
+  (11, 28, `Em trai`),-- Thành viên có ID là 25 trong hộ khẩu số 10 có quan hệ là "Em trai"
+  (11, 29, `Chủ hộ`),
+
+  (12, 30, `Con`),    -- Thành viên có ID là 30 trong hộ khẩu số 12 có quan hệ là "Con"
+  (12, 31, `Chủ hộ`), 
+
+  (13, 32, `Chồng`),  -- Thành viên có ID là 32 trong hộ khẩu số 13 có quan hệ là "Chồng"
+  (13, 33, `Chủ hộ`),
+  (13, 34, `Con),     -- Thành viên có ID là 34 trong hộ khẩu số 13 có quan hệ là "Con"
+
+  (14, 35, `Con`),    -- Thành viên có ID là 35 trong hộ khẩu số 14 có quan hệ là "Con"
+  (14, 36, `Chủ hộ`),
+
+  (15, 37, `Vợ`),     -- Thành viên có ID là 37 trong hộ khẩu số 15 có quan hệ là "Vợ"
+  (15, 38, `Chủ hộ`),
+
+  (16, 39, `Chủ hộ`),
+  (16, 40, `Em gái`),  -- Thành viên có ID là 40 trong hộ khẩu số 10 có quan hệ là "Em gái"
+
+  (17, 41, `Chủ hộ`), 
+  (17, 42, `Vợ`),     -- Thành viên có ID là 42 trong hộ khẩu số 17 có quan hệ là "Con"
+
+  (18, 43, `Chủ hộ`), 
+  (18, 44, `Vợ`),     -- Thành viên có ID là 44 trong hộ khẩu số 18 có quan hệ là "Vợ"
+
+  (19, 45, `Chủ hộ`), 
+  (19, 46, `Con`),    -- Thành viên có ID là 46 trong hộ khẩu số 19 có quan hệ là "Con"
+  (19, 47, `Con`),    -- Thành viên có ID là 47 trong hộ khẩu số 19 có quan hệ là "Con"
+ 
+  (20, 48, `Chủ hộ`), 
+  (20, 49, `Chồng`),  -- Thành viên có ID là 49 trong hộ khẩu số 20 có quan hệ là "Chồng"
+  (20, 50, `Em gái`), -- Thành viên có ID là 50 trong hộ khẩu số 20 có quan hệ là "Em gái"
+  (20, 51, `Con`),    -- Thành viên có ID là 51 trong hộ khẩu số 10 có quan hệ là "Con"
+  (20, 52, `Con`);    -- Thành viên có ID là 52 trong hộ khẩu số 20 có quan hệ là "Con"
