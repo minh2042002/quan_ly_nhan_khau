72cfc0c62983ddc4276edfa3d590d26cbbcf316a
geckoyuil157 <quangduy15hp07@gmail.com>
2023-07-01 16:28:37 +0700

    update

--- database/chuho.sql
+++ database/chuho.sql
@@ -1,20 +1,20 @@
 INSERT INTO `chu_ho` (`MaHo`, `IDChuHo`)
 VALUES
-  (1, 3),  -- Chủ hộ của hộ khẩu số 1 có ID là 1
-  (2, 6),  -- Chủ hộ của hộ khẩu số 2 có ID là 5
-  (3, 9),  -- Chủ hộ của hộ khẩu số 3 có ID là 8
-  (4, 12), -- Chủ hộ của hộ khẩu số 4 có ID là 11
-  (5, 14), -- Chủ hộ của hộ khẩu số 5 có ID là 13
+  (1, 3),  -- Chủ hộ của hộ khẩu số 1 có ID là 3
+  (2, 6),  -- Chủ hộ của hộ khẩu số 2 có ID là 6
+  (3, 9),  -- Chủ hộ của hộ khẩu số 3 có ID là 9
+  (4, 12), -- Chủ hộ của hộ khẩu số 4 có ID là 12
+  (5, 14), -- Chủ hộ của hộ khẩu số 5 có ID là 14
   (6, 15), -- Chủ hộ của hộ khẩu số 6 có ID là 15
   (7, 17), -- Chủ hộ của hộ khẩu số 7 có ID là 17
   (8, 19), -- Chủ hộ của hộ khẩu số 8 có ID là 19
-  (9, 23), -- Chủ hộ của hộ khẩu số 9 có ID là 21
-  (10, 26),-- Chủ hộ của hộ khẩu số 10 có ID là 24
-  (11, 29),-- Chủ hộ của hộ khẩu số 11 có ID là 27
-  (12, 31),-- Chủ hộ của hộ khẩu số 12 có ID là 30
-  (13, 33),-- Chủ hộ của hộ khẩu số 13 có ID là 32
-  (14, 36),-- Chủ hộ của hộ khẩu số 14 có ID là 35
-  (15, 38),-- Chủ hộ của hộ khẩu số 15 có ID là 37
+  (9, 23), -- Chủ hộ của hộ khẩu số 9 có ID là 23
+  (10, 26),-- Chủ hộ của hộ khẩu số 10 có ID là 26
+  (11, 29),-- Chủ hộ của hộ khẩu số 11 có ID là 29
+  (12, 31),-- Chủ hộ của hộ khẩu số 12 có ID là 31
+  (13, 33),-- Chủ hộ của hộ khẩu số 13 có ID là 33
+  (14, 36),-- Chủ hộ của hộ khẩu số 14 có ID là 36
+  (15, 38),-- Chủ hộ của hộ khẩu số 15 có ID là 38
   (16, 39),-- Chủ hộ của hộ khẩu số 16 có ID là 39
   (17, 41),-- Chủ hộ của hộ khẩu số 17 có ID là 41
   (18, 43),-- Chủ hộ của hộ khẩu số 18 có ID là 43
