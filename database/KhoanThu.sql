bceb8f25d24a973aededbe8267d145ea43df7c02
P3jp12022002 <137709657+P3jp12022002@users.noreply.github.com>
2023-07-01 23:25:03 +0700

    Add files via upload

---
+++ database/KhoanThu.sql
@@ -0,0 +1,13 @@
+INSERT INTO `khoan_thu` (`MaKhoanThu`, `TenKhoanThu`, `SoTien`, `LoaiKhoanThu`) VALUES
+(1,'Phí sinh hoạt năm 2023',66000,1),
+(2,'Phí sinh hoạt năm 2024',66000,1),
+(3,'Ủng hộ quỹ vì người nghèo',10000,0),
+(4,'Cứu trợ đồng bào bị ảnh hưởng bởi thiên tai, bão lụt', 10000,0),
+(5, 'Ủng hộ nạn nhân chất độc da cam', 10000,0),
+(6, 'Ủng hộ ngày Tết thiếu nhi', 10000,0),
+(7, 'Ủng hộ ngày thương binh liệt sỹ 27/7', 10000,0),
+(8, 'Ủng hộ phong trào Tết nhân ái', 10000,0),
+(9, 'Ủng hộ quỹ Vì biển, đảo Việt Nam', 10000,0),
+(10, 'Ủng hộ quỹ Trái tim cho em', 10000,0),
+(11, 'Ủng hộ quỹ khuyến học', 10000,0),
+(12, 'Ủng hộ quỹ vaccine', 10000,0);
---
+++ database/Users.sql
@@ -0,0 +1,7 @@
+INSERT INTO `users` (`ID`, `username`, `passwd`) VALUES
+(1, 'admin', '1'),
+(2,'duy01','1111'),
+(3,'dung02','113'),
+(4,'son00008','1113'),
+(5,'minhssss','123456'),
+(6,'phong112','112');
